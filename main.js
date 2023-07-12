7400f6457e0e8eebd3c49810fcc7bf6523ae2b82
RODRIGO NUNES CAMARGO <42442741+Rodrigocamarg0@users.noreply.github.com>
2023-07-12 20:25:11 -0300

    Enable cors

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const app = express()
 const port = process.env.PORT || 3000
 require('dotenv').config()
 
+const cors = require('cors');
+
+
+app.use(cors());
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.static('rotas')) 
